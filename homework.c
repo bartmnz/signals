de64c7a19f07223821b4b9da8c9fa772695b503b
Sean Bartholomew <bartmnz@gmail.com>
2016-05-06 19:40:05 -0400

    implemented optargs

--- homework.c
+++ homework.c
@@ -30,8 +30,48 @@ void handler(int signum, siginfo_t *siginfo, void* context){
 
 
 
-int main(void){
-	
+int main(int argc, char* argv[]){
+	memset(&number, '\0', sizeof(number));
+	number.next = 1;
+	number.increment = 2;
+	number.max = UINT_MAX;
+	int var = 0;
+	int opt, sflag, rflag, errflag;
+	while ((opt = getopt(argc, argv, "s:e:r:")) != -1){
+		switch (opt){
+		case 's':
+			sflag = 1;
+			if (rflag == 1){
+				errflag = 1;
+			}
+			var = atoi(optarg);
+			number.next = var & 1 ? var : var + 1;
+			break;
+		case 'e':
+			var = atoi(optarg);
+			if ( var > 2){
+				number.max = (unsigned int) var <= number.max ? (unsigned int) var : number.max;
+			}
+			break;
+		case 'r':
+			rflag = 1;
+			if (sflag == 1){
+				errflag = 1;
+			}
+			var = atoi(optarg);
+			number.next = var & 1 ? var : var + 1;
+			number.increment = -2;
+			break;
+		default:
+			fprintf(stderr, "ERROR: bad usage/n");
+			exit(0);
+		}
+	}
+	if ( errflag == 1){
+		fprintf(stdout, "ERROR: usage: -s and -r are mutually exclusive.");
+		exit(0);
+	}
+
 	struct sigaction psa;
 	memset (&psa, '\0', sizeof(psa));
 
